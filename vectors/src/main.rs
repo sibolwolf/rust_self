7ce126e8a68a4357d2e44738aa59defcb739a0bc
sibolwolf <hejinlai@foxmail.com>
2017-01-17 01:05:34 +0800

    add vectors

--- vectors/src/main.rs
+++ vectors/src/main.rs
@@ -27,7 +27,7 @@ fn main() {
 
     let v_out = vec![1, 2, 3];
     match v.get(7) {
-        Some(x) => println!("Item 7 is {}", x),
+        Some(x) => println("Item 7 is {}", x),
         None => println!("Sorry, this vector is too short")
 
     }
