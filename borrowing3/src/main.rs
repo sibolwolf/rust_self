1f812c43d83ebf9bfde1b678067e8a8bdc288d30
sibolwolf <hejinlai@foxmail.com>
2017-01-25 23:59:56 +0800

    add borrowing3

---
+++ borrowing3/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let mut v = vec![1, 2, 3];
+
+    for i in &v {
+        println!("{}", i);
+        v.push(34);
+    }
+}
---
+++ borrowing3/target/debug/borrowing3.d
@@ -0,0 +1,5 @@
+/Users/jinlaihe/Documents/Project/Rust/rust_self/borrowing3/target/debug/borrowing3: src/main.rs
+
+/Users/jinlaihe/Documents/Project/Rust/rust_self/borrowing3/target/debug/borrowing3.d: src/main.rs
+
+src/main.rs:
